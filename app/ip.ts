7d892e3983c0f6362eee2e2e3f23e5d2768a5900
Niko <nikodem.zareba00@gmail.com>
2023-11-19 14:57:53 +0000

    first commit

---
+++ app/ip.ts
@@ -0,0 +1,24 @@
+'use server'
+import axios from 'axios'
+
+export const ip = async (ip: string) => {
+  const def = {
+    c: 'San Francisco',
+    f: 'us',
+    i: 'GB'
+  }
+  if (!ip) return def
+  try {
+    const { data } = await axios.get(`https://ipapi.co/${ip}/json`)
+    const city = data.city as string
+    if (city?.match(/dubai|london/i)) return def
+    return {
+      c: data.city as string,
+      f: (data.country_code as string).toLowerCase(),
+      i: (data.country_code as string).toUpperCase()
+    }
+  } catch {
+    //
+  }
+  return def
+}
